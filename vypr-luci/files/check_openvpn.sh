b8fa32499df364120ec88746aea4381a7e296265
kirill.scherbenok <kirill.scherbenok@promwad.com>
2016-02-22 13:45:59 +0300

    Added script for launch in rc.local

--- vypr-luci/files/check_openvpn.sh
+++ vypr-luci/files/check_openvpn.sh
@@ -3,22 +3,33 @@
 SERVICES_DIRECTORY="/etc/init.d/"
 OVPN_FILE_DIRECTORY="/etc/openvpn/"
 SLEEP=60
-[ -z "$2" ] || SLEEP=$2
+[ -z "$1" ] || SLEEP=$1
+VPN=""
 
-if [ $1 == "vypr" ] || [ $1 == "hma" ] ; then
-	SERVER=$( grep "^remote" "$OVPN_FILE_DIRECTORY$1.ovpn" | sed 's/^remote //g' | sed 's/ .*$//g' )
+ping_server () {
+	# SERVER=$( grep "^remote" "$OVPN_FILE_DIRECTORY$VPN.ovpn" | sed 's/^remote //g' | sed 's/ .*$//g' )
+	SERVER="8.8.8.8"
+	ping -w 2 -c 1 ${SERVER} >/dev/null 2>&1
+	if [ ! $? = 0 ]; then
+		$SERVICES_DIRECTORY$VPN restart >/dev/null 2>&1
+	fi
+}
 
-	echo $SERVER
+run () {
+	if [ -z $(pidof openvpn) ] ; then
+		$SERVICES_DIRECTORY$VPN start
+	else
+		ping_server
+	fi
+}
 
-	while true; do
-		ping -w 2 -c 1 ${SERVER} >/dev/null 2>&1
-			if [ ! $? = 0 ]; then
-				echo $SERVICES_DIRECTORY$1
-				$SERVICES_DIRECTORY$1 restart >/dev/null 2>&1
-			fi
-		echo $SLEEP
-		sleep $SLEEP
-	done
-else
-	echo "incorrect params"
-fi
+while true; do
+	if [ $(uci get vypr.general.enabled)=="1" ] && [ $(uci get hma.general.enabled)=="0" ] ; then
+		VPN="vypr"
+		run 
+	elif [ $(uci get vypr.general.enabled)=="0" ] && [ $(uci get hma.general.enabled)=="1" ] ; then
+		VPN="hma"
+		run
+	fi
+	sleep $SLEEP
+done
